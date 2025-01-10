74c10334a7a303c1b671ccf06bfca95bd7f11899
Matej <ruzicka.maty@gmail.com>
2025-01-10 23:10:53 +0100

    backported to 1.20.2

---
+++ common/src/main/java/com/mars/slabstoblocks/Constants.java
@@ -0,0 +1,11 @@
+package com.mars.slabstoblocks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "slabstoblocks";
+	public static final String MOD_NAME = "Craft Slabs Back Into Blocks";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ forge/src/main/java/com/mars/slabstoblocks/platform/ForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package com.mars.slabstoblocks.platform;
+
+import com.mars.slabstoblocks.platform.services.IPlatformHelper;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
