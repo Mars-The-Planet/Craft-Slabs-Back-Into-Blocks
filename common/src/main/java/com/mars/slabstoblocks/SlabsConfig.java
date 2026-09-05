83b45e0bef6084b9333b96e3e39412290cdfcce8
Mars-The-Planet <microsoft.maty@gmail.com>
2026-09-05 17:16:05 +0200

    last update

--- common/src/main/java/com/mars/slabstoblocks/SlabsConfig.java
+++ common/src/main/java/com/mars/slabstoblocks/SlabsConfig.java
@@ -6,7 +6,8 @@ import com.mars.deimos.config.DeimosConfig;
 import java.util.List;
 
 public class SlabsConfig extends DeimosConfig {
-    @Entry public static List<String> slabBlockList = Lists.newArrayList("minecraft:acacia_slab, minecraft:acacia_planks", 
+    @Entry public static boolean show_wishful_recipes_message = true;
+    @Entry public static List<String> slabBlockList = Lists.newArrayList("minecraft:acacia_slab, minecraft:acacia_planks",
         "minecraft:andesite_slab, minecraft:andesite",
         "minecraft:bamboo_mosaic_slab, minecraft:bamboo_mosaic",
         "minecraft:bamboo_slab, minecraft:bamboo_planks, -",
